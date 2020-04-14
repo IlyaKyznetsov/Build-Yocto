12a741fcb023b1bc764fa7dd10dd806243bc4b6e
Ilya <kyzmanix@gmail.com>
2020-04-14 13:39:14 +0300

    Добавлен репозиторий xilinx

--- ubuntu-yocto-build-16.04/layers/syncAll.sh
+++ ubuntu-yocto-build-16.04/layers/syncAll.sh
@@ -6,9 +6,9 @@ allClonePull()
     local PATH_TO_CLONE_DIR="${1}"
     local BRANCH="${2}"
     local REPOSITORIES="${*:3}"
-#     echo "--- Clone/pull to: ${PATH_TO_CLONE_DIR}"
-#     echo "--- Repositories: ${REPOSITORIES[*]}"
-#     echo "--- Set branch: ${BRANCH}"
+    echo "--- Clone/pull to: ${PATH_TO_CLONE_DIR}"
+    echo "--- Repositories: ${REPOSITORIES[*]}"
+    echo "--- Set branch: ${BRANCH}"
     if [ ! -d ${PATH_TO_CLONE_DIR} ]; then
         mkdir -p ${PATH_TO_CLONE_DIR}
     fi
@@ -17,7 +17,7 @@ allClonePull()
         echo "--- Repository: ${item} Branch: ${BRANCH}"
         DIR="$(basename $item)"
 	[[ 'git' == "${DIR##*.}" ]] && DIR="${DIR%.*}"
-        if [ ! -d ${DIR} ]; then
+        if [ ! -d ${DIR}/.git ]; then
             git clone -b ${BRANCH} $item
         else
             cd $DIR
@@ -37,8 +37,8 @@ else
     echo "--- Branch: ${BRANCH}"
 fi
 
-REPOSITORIES_INTERNET=('git://git.yoctoproject.org/poky' 'git://git.openembedded.org/meta-openembedded' 'git://github.com/meta-qt5/meta-qt5.git' 'https://git.yoctoproject.org/git/meta-freescale')
+REPOSITORIES_INTERNET=('git://git.yoctoproject.org/poky' 'git://git.openembedded.org/meta-openembedded' 'git://github.com/meta-qt5/meta-qt5.git' 'https://git.yoctoproject.org/git/meta-freescale' 'git://git.yoctoproject.org/meta-xilinx')
 REPOSITORIES_CUSTOM=('https://github.com/IlyaKyznetsov/meta-ik.git')
 
 allClonePull 'internet' ${BRANCH} ${REPOSITORIES_INTERNET[*]}
-allClonePull 'custom' ${BRANCH} ${REPOSITORIES_CUSTOM[*]}
+allClonePull 'custom' master ${REPOSITORIES_CUSTOM[*]}
