06aa973ed1065b048a528c712df5e37a4928eea5
Ilya <kyzmanix@gmail.com>
2020-06-05 17:31:30 +0300

    Добавлен yocto-3.1

--- Ubuntu-16.04/buildContainer.sh
+++ Ubuntu-16.04/buildContainer.sh
@@ -2,6 +2,14 @@ CONTAINER_NAME='ubuntu-yocto-build-16.04'
 CONTAINER_USER='build'
 BIN='docker'
 #BIN='podman'
+#if [[ ${BIN}=='podman' ]]; then
+#CONTAINER_USER_LOCAL_HOME="$CURRENT_DIR/../${CONTAINER_NAME}"
+#if [ ! -d "${CONTAINER_USER_LOCAL_HOME}" ]; then
+#    mkdir -p "${CONTAINER_USER_LOCAL_HOME}"
+#fi
+#podman unshare chown -R $(id -u) "${CONTAINER_USER_LOCAL_HOME}"
+#fi
 # sudo usermod --add-subuids 100000-165536 $USER
 # sudo usermod --add-subgids 100000-165536 $USER
+#${BIN} build --no-cache --build-arg "host_uid=689824" --build-arg "host_gid=689824" --build-arg "USER_NAME=$CONTAINER_USER" -t "${CONTAINER_NAME}" .
 ${BIN} build --no-cache --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)" --build-arg "USER_NAME=$CONTAINER_USER" -t "${CONTAINER_NAME}" .
--- Ubuntu-16.04/runContainer.sh
+++ Ubuntu-16.04/runContainer.sh
@@ -5,11 +5,17 @@ CURRENT_DIR="$(dirname $(readlink -f $0))"
 BIN='docker'
 #BIN='podman'
 RAM_MEMORY='6144m'
+RAM_MEMORY='8192m'
 CPU_CORES='1,2,3'
 CONTAINER_USER_LOCAL_HOME="$CURRENT_DIR/../${CONTAINER_NAME}"
 if [ ! -d "${CONTAINER_USER_LOCAL_HOME}" ]; then
     mkdir -p "${CONTAINER_USER_LOCAL_HOME}"
 fi
+
+if [[ ${BIN} == 'podman'  ]]; then
+${BIN} run --privileged --memory-swappiness=0 --memory=${RAM_MEMORY} --user ${CONTAINER_USER} --userns=keep-id -it -v ${CONTAINER_USER_LOCAL_HOME}:/home/${CONTAINER_USER} ${CONTAINER_NAME} /bin/bash
+else
 ${BIN} run --memory-swappiness=0 --cpuset-cpus=${CPU_CORES} --memory=${RAM_MEMORY} --user ${CONTAINER_USER} -it -v ${CONTAINER_USER_LOCAL_HOME}:/home/${CONTAINER_USER}:Z ${CONTAINER_NAME} /bin/bash
+fi
 #${BIN} run --memory=6144m --memory-swappiness=0 --cpuset-cpus=0-2 --user ${USER} --privileged -it -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${LOCAL_PATH}:/home/${USER} ${NAME} /bin/bash
 #${BIN} run --user ${USER} -it -v ${LOCAL_PATH}:/home/${USER}:Z ${NAME} /bin/bash
